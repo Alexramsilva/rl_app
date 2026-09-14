c0ae2beb9a49feecd2a3356008d2a29ea97cf8ec
UNRC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2026-09-14 12:38:58 -0600

    Increase image width for NQH.svg in rl_app.py

--- rl_app.py
+++ rl_app.py
@@ -20,7 +20,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.image("NQH.svg", caption="", width=200)
+st.image("NQH.svg", caption="", width=400)
 st.title("Nopal Quant")
 st.subheader("Reinforcement Learning Trading Agent")
 
